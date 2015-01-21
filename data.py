482d29d163987fd89d2a7bb7e2e74b725896bf37
seba-1511 <seba-1511@hotmail.com>
2015-01-20 20:04:29 -0800

    Implemented Reading of matrix and configuration.

--- data.py
+++ data.py
@@ -7,8 +7,10 @@ import numpy as np
 from random import randint
 from pdb import set_trace as debug
 
+TOTAL_NB_QUBITS = 512
 INSTANCES_DIR = 'plantedInstances/'
 SOLUTIONS_DIR = 'solutionsForInstances/'
+VALUES_START = 4
 
 
 class Solution(object):
@@ -28,7 +30,6 @@ class Instance(object):
         self.nb_sg = nb_sg
         self.id = id
         file = self.load_file()
-        debug()
         self.config = self.read_config(file)
         self.J = self.read_matrix(file)
         self.min_cost = self.read_cost(file)
@@ -42,15 +43,22 @@ class Instance(object):
         )
         directory = os.path.dirname(os.path.abspath(__file__))
         directory = os.path.join(directory, INSTANCES_DIR)
-        return open(directory + filename, 'r').readlines()
-
+        file = open(directory + filename, 'r')
+        return file.readlines()
 
     def read_config(self, file):
-        res = ''
-        file.seek(4, 0)
+        config = []
+        for i in xrange(VALUES_START, self.nb_qubits + VALUES_START):
+            values = file[i].split()
+            config.append(int(values[1]))
+        return np.array(config)
 
     def read_matrix(self, file):
-        pass
+        matrix = np.zeros((TOTAL_NB_QUBITS, TOTAL_NB_QUBITS))
+        for i in xrange(self.nb_qubits + VALUES_START + 2, len(file) - 2):
+            row, col, value = file[i].split()
+            matrix[int(row), int(col)] += int(value)
+        return matrix
 
     def read_cost(self, file):
         pass
