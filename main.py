a58d8391960e8ac6351dd2b8c7fae610ea6af809
seba-1511 <seba-1511@hotmail.com>
2015-02-03 01:44:25 -0800

    Implemented hamming distance and commented some functions.

--- main.py
+++ main.py
@@ -15,11 +15,19 @@ from pdb import set_trace as debug
 
 
 def parse_instance(file):
+    """
+        Given the a file object of an instance loaded with Python,
+        it returns an Instance object of this problem.
+    """
     nb_qubits, nb_sg, id = map(int, re.findall(r'\d+', file))
     return Instance(nb_sg=nb_sg, id=id, nb_qubits=nb_qubits)
 
 
 def get_all_instances(nb_sg=None, nb_qubits=None, id=None):
+    """
+        Returns all the instances in an array.
+        We can specify restrictions to load only a subset of them.
+    """
     instances = []
     sg_condition = 'Nsg' + str(nb_sg) if nb_sg is not None else ''
     qb_condition = 'Nq' + str(nb_qubits) if nb_qubits is not None else ''
@@ -30,6 +38,12 @@ def get_all_instances(nb_sg=None, nb_qubits=None, id=None):
                     id_condition in file):
                 instances.append(parse_instance(file))
     return instances
+
+
+def hamming_distance(str1, str2):
+    """Implements the Hamming distance between 2 strings."""
+    assert(len(str1) == len(str2))
+    return sum([1 if x != y else 0 for x, y in zip(str1, str2)])
 
 if __name__ == '__main__':
     data = Instance(nb_sg=504, id=199)
