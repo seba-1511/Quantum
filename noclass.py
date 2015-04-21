83f6d669840107fd0d0bf9d658e0a3e86aeea443
seba-1511 <seba-1511@hotmail.com>
2015-04-21 00:43:58 -0700

    Implemented grid search for temperatures.

--- noclass.py
+++ noclass.py
@@ -25,31 +25,33 @@ print 'Random Seed: ', RND_SEED
 
 
 # @profile
-def run():
+def run(problem_id, given_temps):
     time_to_solution = time.time()
     # initialization of loop variables:
     add, sub = 0, 0
     random = Random(RND_SEED).random
     randrange = Random(RND_SEED).randrange
     choice = Random(1234).choice
-    instance = LookupInstance(id=0, nb_sg=420)
+    instance = LookupInstance(id=problem_id, nb_sg=420)
+    # instance = LookupInstance(id=0, nb_sg=420)
     J = instance.J
     n_sweeps = N_SWEEPS
     cost = None
-    val = (-1, 1)
+    possible_values = (-1, 1)
     # generate_temperatures ******************
-    temperatures = [2, 1, .5]
+    # temperatures = [2, 1, .5]
+    temperatures = given_temps
     # ****************************************
 
     # get_accept_probs() *****************
     accept_probs = tuple(tuple(exp(d / T)
                                for d in xrange(-DIFF_RANGE, 0)) for T in temperatures)
     # ************************************
-    print 'Time to config: ', time.time() - time_to_solution
+    # print 'Time to config: ', time.time() - time_to_solution
     # while cost != instance.min_cost:
     for i in xrange(5):
         #: Init variables for annealing
-        solution = [choice(val) for i in xrange(TOTAL_NB_QUBITS)]
+        solution = [choice(possible_values) for i in xrange(TOTAL_NB_QUBITS)]
         cost = instance.get_cost
 
         #: Run the annealing process
@@ -76,8 +78,8 @@ def run():
                         solution[swap] *= -1
                 if cost <= instance.min_cost:
                     break
-            print T, ': ', 'Current best: ', cost, '/', instance.min_cost
-            print 'timing', time.time() - start
+            # print T, ': ', 'Current best: ', cost, '/', instance.min_cost
+            # print 'timing', time.time() - start
             if cost <= instance.min_cost:
                 break
         if cost == instance.min_cost:
@@ -85,13 +87,53 @@ def run():
     end = time.time() - time_to_solution
 
     print 'Time to solution: ', end
+    return end
+
+from multiprocessing import Pool
+
+
+def drange(start, stop, step):
+    res = []
+    r = start
+    while r < stop:
+        res.append(r)
+        r += step
+    return res
+
+
+def explore(temp):
+    # ids = xrange(0, 500, 50)
+    ids = [0, 100]
+    times = [run(i, temp) for i in ids]
+    return sum(times)/len(times)
+
 
 if __name__ == '__main__':
-    run()
+    temps = [
+        [2, 1, .5],
+        [2, 1, .75, .5],
+        [3, 2, 1, .5],
+        [3, 2.5, 2, 1, .5],
+        [2, 1.5, 1, .5],
+        [3, 2, 1.5, 1, .5],
+        [4, 3, 2, 1.5, 1, .5],
+        [5, 4, 3, 2, 1.5, 1, .5],
+        [3.2, 2.7, 2, 1.5, 1, .5],
+        drange(.5, 3, .1)[::-1],
+        drange(.5, 3, .2)[::-1],
+        drange(.5, 3, .25)[::-1],
+        drange(.5, 3, .33)[::-1],
+        drange(.5, 3, .4)[::-1],
+        drange(.5, 3, .5)[::-1],
+    ]
+    pool = Pool(processes=7)
+    results = pool.map(explore, temps)
+    for i, r in enumerate(results):
+        print 'Entry: ', i, ', average: ', r
 
 # To improve:
 # - To find diff: if sol[i] == sol[j] -> add J[i, j] else substract.
 # - Manually calculate the exp probs
 # - change so that diff = new - old
-# - Replace sum with for loop, to see what happens
+# - Replace sum with for loop -> About the same, slightly slower
 # - Find best configurations and temperatures
