ab80975d6c353efb17dba070b6f13519c448aa12
seba-1511 <seba-1511@hotmail.com>
2015-06-08 15:31:03 -0700

    Added quantum annealer readme.

---
+++ quantumAnnealing/README.md
@@ -0,0 +1,11 @@
+## Quantum Annealing
+Implementation of a quantum annealer simulator.
+
+#### runge_kutta.py
+A 4th order Runge-Kutta algorithm for vector equations.
+
+#### performance.py
+Profiling utilities.
+
+#### graph/
+Graphing utilities.
--- quantumAnnealing/utils.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-
-
-def hamming_distance(str1, str2):
-    """Implements the Hamming distance between 2 strings."""
-    assert(len(str1) == len(str2))
-    return sum([1 if x != y else 0 for x, y in zip(str1, str2)])
