590dfa84c7fcaab4015dfdcf78c88467d201ae75
seba-1511 <seba-1511@hotmail.com>
2015-06-14 14:58:13 -0700

    Added quantum adiabatic annealing procedure script.

---
+++ quantumAnnealing/annealer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def dot_scal_sparse(scal, sparse):
+    pass
+
+if __name__ == '__main__':
+
+    A = lambda t: 2 * t
+    b = lambda t: 3 * t
+
+
+    H = lambda t:  A(t) * H_d + B(t) * H_p
