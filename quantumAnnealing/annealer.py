befdda3d48f17c8cb1d777d96d51dd6cb0cb54bf
seba-1511 <seba-1511@hotmail.com>
2015-06-15 16:05:51 -0700

    Implemented a correct Driver matrix, with caching to files.

--- quantumAnnealing/annealer.py
+++ quantumAnnealing/annealer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import cPickle as pk
 from copy import deepcopy
 
 
@@ -39,19 +41,57 @@ def dat_sparse_vec(sparse, vec):
         res.append(val)
     return res
 
-if __name__ == '__main__':
-    NB_QUBITS = 10
 
-    A = lambda t: 2 * t
-    B = lambda t: 3 * t
-    H_d = {}
-    H_p = {x: {x: 1} for x in xrange(NB_QUBITS)}
+def bit_difference(nb1, nb2, diff):
+    s = sum([a != b for a, b in zip(nb1, nb2)])
+    return s == diff
+    if len(nb1) <= 0:
+        return diff >= 0
+    elif diff <= 0:
+        return False
+    else:
+        diff -= 0 if nb1[0] == nb2[0] else 1
+        return bit_difference(nb1[1:], nb2[1:], diff)
+
+
+def driver_matrix(nb_qubits):
+    bnb_qubits = bin(nb_qubits)
+    if os.path.isfile(str(nb_qubits) + 'Hd.bin') and False:
+        with open(str(nb_qubits) + 'Hd.bin') as f:
+            res = pk.load(f)
+    else:
+        res = {
+            x: {} for x in xrange(nb_qubits)
+        }
+        for row in xrange(nb_qubits):
+            for col in xrange(nb_qubits):
+                brow = bin(row)[2:]
+                bcol = bin(col)[2:]
+                diff = len(bnb_qubits) - len(bcol)
+                bcol = ('0' * diff) + bcol
+                diff = len(bnb_qubits) - len(brow)
+                brow = ('0' * diff) + brow
+                if bit_difference(brow, bcol, 1):
+                    res[row][col] = 1
+        with open(str(nb_qubits) + 'Hd.bin', 'wb') as f:
+            pk.dump(res, f)
+    return res
+
+if __name__ == '__main__':
+    NB_QUBITS = 5
+    NB_ENTRIES = 2 ** NB_QUBITS
+
+    A = lambda t: t
+    B = lambda t: 1.0 - t
+    H_d = driver_matrix(NB_ENTRIES)
+    H_p = {x: {x: 1} for x in xrange(NB_ENTRIES)}
 
     H = lambda t: add_sparse(
         dot_scal_sparse(A(t), H_d), dot_scal_sparse(B(t), H_p))
 
-    print dot_scal_sparse(3.0, H_p)
+    print H_d
 
     # DONE: Represent sparse matrices as dict of dict: a[row][col] = val
     # Even though lists are faster, to add and multiply it will be practical
     # to have the col/row separation
+    # There should be NB_QUBITS neighbors for the driver_matrix
