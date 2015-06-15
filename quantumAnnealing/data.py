7c3c2a0162d2858cd5683c331e0e75e458934eed
seba-1511 <seba-1511@hotmail.com>
2015-06-15 16:51:35 -0700

    Implemnted an example experiment of annealing.

---
+++ quantumAnnealing/data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from random import randint
+
+def load_instance(nb_qubits, id, sg):
+    pass
+
+
+def generate_instance(size):
+    res = {
+        x: {
+            x: randint(0, size)
+        } for x in xrange(size)
+    }
+
+    return res
---
+++ quantumAnnealing/solver.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runge_kutta import (
+    RK,
+    dot_vec_vec,
+)
+from annealer import (
+    driver_matrix,
+    add_sparse,
+    dot_scal_sparse,
+    dot_sparse_vec,
+)
+from data import (
+    load_instance,
+    generate_instance,
+)
+
+if __name__ == '__main__':
+    NB_QUBITS = 5
+    NB_ENTRIES = 2 ** 5
+    T = 20.0
+    A = lambda t: 1.0 - t / T
+    B = lambda t: t / T
+
+    H_p = generate_instance(NB_ENTRIES)
+    H_d = driver_matrix(NB_ENTRIES)
+
+    H = lambda t: add_sparse(
+        dot_scal_sparse(A(t), H_d),
+        dot_scal_sparse(B(t), H_p)
+    )
+    F = lambda t: dot_scal_sparse(1j, H(t))
+
+    y_dot = RK(F)
+    init = [1] * NB_ENTRIES
+    dt = 0.01
+    t = 0
+
+    while t < T:
+        val = y_dot(y=init, t=t, dt=dt)
+        t += dt
+        init = val
+
+    print 'Problem: ', [H_p[i][i] for i in xrange(NB_ENTRIES)]
+    print 'Probs: ', [abs(a) ** 2 for a in init]
+
