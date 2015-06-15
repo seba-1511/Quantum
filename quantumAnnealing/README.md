16e29d3d64db7a244dc16c38a45cf6a8cdae4a5c
seba-1511 <seba-1511@hotmail.com>
2015-06-14 17:05:18 -0700

    Implemented methods for sparse matrices addition and scalar product.

--- quantumAnnealing/README.md
+++ quantumAnnealing/README.md
@@ -1,6 +1,9 @@
 ## Quantum Annealing
 Implementation of a quantum annealer simulator.
 
+#### annealer.py
+An implementation of the quantum adiabatic annealing algorithm.
+
 #### runge_kutta.py
 A 4th order Runge-Kutta algorithm for vector equations.
 
--- quantumAnnealing/annealer.py
+++ quantumAnnealing/annealer.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from copy import deepcopy
+
+
+def add_sparse(s1, s2):
+    """
+        Return the addition of two sparse matrices.
+    """
+    res = deepcopy(s1)
+    for row in s2.keys():
+        for col in s2[row].keys():
+            res[row][col] = s1[row][col] if row in s1 and col in s1[row] else 0
+            res[row][col] += s2[row][col]
+    return res
+
+
 def dot_scal_sparse(scal, sparse):
-    pass
+    """
+        Returns the product of a scalar with a sparse matrix.
+    """
+    res = deepcopy(sparse)
+    for row in res.keys():
+        for col in res[row].keys():
+            res[row][col] *= scal
+    return res
 
 if __name__ == '__main__':
+    NB_QUBITS = 10
 
     A = lambda t: 2 * t
-    b = lambda t: 3 * t
+    B = lambda t: 3 * t
+    H_d = {}
+    H_p = {x: {x: 1} for x in xrange(NB_QUBITS)}
 
+    H = lambda t: add_sparse(
+        dot_scal_sparse(A(t), H_d), dot_scal_sparse(B(t), H_p))
 
-    H = lambda t:  A(t) * H_d + B(t) * H_p
+    print dot_scal_sparse(3.0, H_p)
+
+    # TODO: Represent sparse matrices as dict of dict: a[row][col] = val
+    # Even though lists are faster, to add and multiply it will be practical
+    # to have the col/row separation
